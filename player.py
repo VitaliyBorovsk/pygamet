27d2a417ae49a065798fcecb59479647127a5163
VitaliyBorovsk <borovskyv2010@gmail.com>
2026-02-15 16:55:00 +0300

    trgf

---
+++ player.py
@@ -0,0 +1,57 @@
+import pygame as pg
+
+
+
+TILE_SCALE = 1
+class Player(pg.sprite.Sprite):
+    def __init__(self, map_width, map_height):
+        super(Player, self).__init__()
+
+        self.image = pg.Surface((50,50))
+        self.image.fill("red")
+
+        self.rect = self.image.get_rect()
+        self.rect.center = (200, 100)  # Начальное положение персонажа
+
+        # Начальная скорость и гравитация
+        self.velocity_x = 0
+        self.velocity_y = 0
+        self.gravity = 2
+        self.is_jumping = False
+        self.map_width = map_width * TILE_SCALE
+        self.map_height = map_height * TILE_SCALE
+
+    def update(self, platforms):
+        keys = pg.key.get_pressed()
+        if keys[pg.K_SPACE] and not self.is_jumping:
+            self.jump()
+        if keys[pg.K_a]:
+            self.velocity_x = -10
+        elif keys[pg.K_d]:
+            self.velocity_x = 10
+        else:
+            self.velocity_x = 0
+        new_x = self.rect.x + self.velocity_x
+        if 0 <= new_x <= self.map_width - self.rect.width:
+            self.rect.x = new_x
+
+        self.velocity_y += self.gravity
+        self.rect.y += self.velocity_y
+
+        for platform in platforms:
+            if platform.rect.collidepoint(self.rect.midbottom):
+                self.rect.bottom = platform.rect.top
+                self.velocity_y = 0
+                self.is_jumping = False
+            if platform.rect.collidepoint(self.rect.midtop):
+                self.rect.top = platform.rect.bottom
+                self.velocity_y = 0
+            if platform.rect.collidepoint(self.rect.midright):
+                self.rect.right = platform.rect.left
+
+            if platform.rect.collidepoint(self.rect.midleft):
+                self.rect.left = platform.rect.right
+
+    def jump(self):
+        self.velocity_y = -45
+        self.is_jumping = True
